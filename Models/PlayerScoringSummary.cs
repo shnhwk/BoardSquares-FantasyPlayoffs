0028a46cd6c0a830e89d3e2f355f2055aa5d1f43
Shane Hawk <shnhwk@gmail.com>
2020-12-18 15:23:12 -0500

    Padding on main player rows, player team to player scores table, full row clickable.

--- Models/PlayerScoringSummary.cs
+++ Models/PlayerScoringSummary.cs
@@ -6,6 +6,7 @@
         public string PlayerPosition { get; set; }
         public string PlayerFirstName { get; set; }
         public string PlayerLastName { get; set; }
+        public string PlayerTeam { get; set; }
         public int WildCard { get; set; }
         public int Divisional { get; set; }
         public int Conference { get; set; }
